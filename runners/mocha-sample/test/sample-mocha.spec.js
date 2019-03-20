8b6d8382888179fcad6b2788516dbb91f3b5f0fb
Bernard Labno <bernard.labno@pjwstk.edu.pl>
2019-03-20 12:08:23 +0100

    Mocha sample

---
+++ runners/mocha-sample/test/sample-mocha.spec.js
@@ -0,0 +1,9 @@
+const expect = require('chai').expect;
+
+const add = (a, b) => a + b;
+
+describe('add', () => {
+  describe('when a is 2 and b is 3', () => {
+    it('should return 5', () => expect(add(2, 3)).to.equal(5));
+  });
+});
